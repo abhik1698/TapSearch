03eea807979a10070f7e9f017cbe5db2210df253
abhik1698 <abhik1698@gmail.com>
2019-12-01 03:15:43 +0530

    Prototype ready

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ def home():
   global paragraphs
   if request.method == 'POST':
     paragraphs = request.form['para'].split("\r\n\r\n")
+    paragraphs.reverse()
+    paragraphs.append(' ')
     
-    return render_template('paras.html', paragraphs=paragraphs, len=len(paragraphs), flen=-1)
+    return render_template('paras.html', paragraphs=paragraphs, plen=len(paragraphs)-1, flen=-1)
   else:
     return render_template('home.html')
 
@@ -20,15 +22,22 @@ def search():
   if request.method == 'POST':
     global paragraphs
     key = request.form['key'].strip().lower()
-    found = []
+    found, plen = [], len(paragraphs)-1
+    
+    if plen < 10:
+      for i in range(plen):      
+        for v in paragraphs[i].split(' '):
+          if key == v.lower():
+            found.append(i+1)
+            break
+    else: #To check top 10 paragraphs
+      for i in range(10):      
+        for v in paragraphs[i].split(' '):
+          if key == v.lower():
+            found.append(i+1)
+            break
 
-    for i in range(len(paragraphs)):      
-      for v in paragraphs[i].split(' '):
-        if key == v.lower():
-          found.append(i)
-          break
-
-    return render_template('paras.html', found=found, flen=len(found), paragraphs=paragraphs, len=len(paragraphs))
+    return render_template('paras.html', found=found, flen=len(found), paragraphs=paragraphs, plen=plen)
 
   else:
     return render_template('home.html')
