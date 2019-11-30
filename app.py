f7a4c42160ce2fd2d6a10129749b6e112d8e147c
abhik1698 <abhik1698@gmail.com>
2019-12-01 02:32:54 +0530

    GET POST

--- app.py
+++ app.py
@@ -10,23 +10,28 @@ def home():
   if request.method == 'POST':
     paragraphs = request.form['para'].split("\r\n\r\n")
     
-    return render_template('id.html', paragraphs=paragraphs, len=len(paragraphs), flen=-1)
+    return render_template('paras.html', paragraphs=paragraphs, len=len(paragraphs), flen=-1)
   else:
     return render_template('home.html')
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-  global paragraphs
-  key = request.form['key'].strip().lower()
-  found = []
+  if request.method == 'POST':
+    global paragraphs
+    key = request.form['key'].strip().lower()
+    found = []
 
-  for i in range(len(paragraphs)):
-    for v in paragraphs[i].split(' '):
-      if key == v.lower():
-        found.append(i)
-        break
+    for i in range(len(paragraphs)):
+      for v in paragraphs[i].split(' '):
+        if key == v.lower():
+          found.append(i)
+          break
 
-  return render_template('id.html', found=found, flen=len(found), paragraphs=paragraphs, len=len(paragraphs))
+    return render_template('paras.html', found=found, flen=len(found), paragraphs=paragraphs, len=len(paragraphs))
+
+  else:
+    return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
